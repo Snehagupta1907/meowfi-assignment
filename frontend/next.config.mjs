904818f78bab6f8aff5642b0ca790bca6c4d17e9
sneha gupta <snehagupta98930@gmail.com>
2025-05-19 19:26:24 +0530

    update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ["cdn.getro.com"],
+    },
+    typescript: {
+      ignoreBuildErrors: true, // Skip TypeScript type checking
+    },
+    eslint: {
+      ignoreDuringBuilds: true, // Skip ESLint checks during builds
+    },
+  };
+  
+  export default nextConfig;
